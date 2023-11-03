bcd584caca46baa97e81423d8ebc8f9ab5f3e3b5
Kote <km@projectn.co>
2023-11-03 11:26:20 -0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 import setuptools
 from setuptools import setup
 
-requires = ['boto3', 'botocore']
-python_requires = '>=3'
+requires = ["boto3", "botocore"]
+python_requires = ">=3"
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='granica-sdk',
+    name="granica-sdk",
     packages=setuptools.find_packages(),
-    version='2.1.9',
-    description='Granica Python SDK',
+    version="2.2.0",
+    description="Granica Python SDK",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Project N',
+    long_description_content_type="text/markdown",
+    author="Project N",
     install_requires=requires,
     classifiers=[
         "Programming Language :: Python :: 3",
