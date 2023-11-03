c828fc39c46df8e4126889152b5d8800fef72cab
Kote <km@projectn.co>
2023-11-03 11:25:29 -0400

    feat: backport changes re: scheming and args

--- granica/__init__.py
+++ granica/__init__.py
@@ -9,86 +9,95 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-import json
-
-from collections import defaultdict
-from os import environ as _environ
-from random import choice
-from threading import Lock 
+import sys
+from os import environ
 from urllib.parse import urlsplit
 from urllib.parse import urlunsplit
 
-import urllib3
-from boto3 import Session as _Session
-from botocore.auth import SigV4Auth as _SigV4Auth
-from botocore.awsrequest import AWSRequest as _AWSRequest
-from botocore.config import Config as _Config
-from botocore.exceptions import UnknownEndpointError
+from boto3 import Session as Boto3Session
+from botocore.config import Config as BotoCoreConfig
 
 from .bolt_router import BoltRouter, get_region, get_availability_zone_id
 
+BOLT_ENDPOINT_UPDATE_INTERVAL = 10
+
+
 # Override Session Class
-class Session(_Session):
+class Session(Boto3Session):
     def __init__(self):
         super(Session, self).__init__()
 
         # Load all of the possibly configuration settings
-        region = _environ.get('BOLT_REGION')
+        region = environ.get("BOLT_REGION")
         if region is None:
             try:
                 region = get_region()
             except Exception as e:
+                print(
+                    "BOLT_REGION environment variable is not set, and could not be automatically determined."
+                )
                 pass
-        custom_domain = _environ.get('GRANICA_CUSTOM_DOMAIN')
+        custom_domain = environ.get("GRANICA_CUSTOM_DOMAIN")
         if custom_domain is None:
-            custom_domain = _environ.get('BOLT_CUSTOM_DOMAIN')
-        service_url = _environ.get('BOLT_URL')
-        bolt_hostname = _environ.get('BOLT_HOSTNAME')
+            custom_domain = environ.get("BOLT_CUSTOM_DOMAIN")
+        service_url = environ.get("BOLT_URL")
         hostname = None
 
         if custom_domain is not None and region is not None:
-            scheme = 'https' 
-            service_url = f"quicksilver.{region}.{custom_domain}"
+            scheme = "https"
+            service_url = "{}://quicksilver.{}.{}".format(scheme, region, custom_domain)
             hostname = f"bolt.{region}.{custom_domain}"
         elif service_url is not None:
             scheme, service_url, _, _, _ = urlsplit(service_url)
             if "{region}" in service_url:
                 if region is None:
-                    raise ValueError(f'Granica URL {service_url} requires region to be specified')
-                service_url = service_url.replace('{region}', region)
+                    raise ValueError(
+                        f"Granica URL {service_url} requires region to be specified"
+                    )
+                service_url = service_url.replace("{region}", region)
         else:
             # must define either `custom_domain` or `url`
             raise ValueError(
-                'Granica settings could not be found.\nPlease expose GRANICA_CUSTOM_DOMAIN')
+                "Granica settings could not be found.\nPlease expose GRANICA_CUSTOM_DOMAIN"
+            )
 
-        az_id = None
-        try:
-            az_id = get_availability_zone_id()
-        except Exception as e:
-            pass
+        az_id = environ.get("BOLT_AZ_ID")
+        if az_id is None:
+            try:
+                az_id = get_availability_zone_id()
+            except Exception as e:
+                print(
+                    "BOLT_AZ_ID environment variable is not set, and could not be automatically determined."
+                )
+                pass
 
-        self.bolt_router = BoltRouter(scheme, service_url, hostname, region, az_id, update_interval=30)
-        self.events.register_last('before-send.s3', self.bolt_router.send)
+        self.bolt_router = BoltRouter(
+            scheme=scheme,
+            quicksilver_api_base_url=service_url,
+            hostname=hostname,
+            region=region,
+            az_id=az_id,
+            update_interval=BOLT_ENDPOINT_UPDATE_INTERVAL,
+        )
+        self.events.register_last("before-send.s3", self.bolt_router.send)
 
     def client(self, *args, **kwargs):
-        if kwargs.get('service_name') == 's3' or 's3' in args:
-            kwargs['config'] = self._merge_bolt_config(kwargs.get('config'))
+        if kwargs.get("service_name") == "s3" or "s3" in args:
+            kwargs["config"] = self._merge_bolt_config(kwargs.get("config"))
             return self._session.create_client(*args, **kwargs)
         else:
             return self._session.create_client(*args, **kwargs)
 
-    def _merge_bolt_config(self, client_config) :
+    def _merge_bolt_config(self, client_config):
         # Override client config
-        bolt_config = _Config(
-            s3={
-                'addressing_style': 'path',
-                'signature_version': 's3v4'
-            }
+        bolt_config = BotoCoreConfig(
+            s3={"addressing_style": "path", "signature_version": "s3v4"}
         )
         if client_config is not None:
             return client_config.merge(bolt_config)
         else:
             return bolt_config
+
 
 # The default Boto3 session; autoloaded when needed.
 DEFAULT_SESSION = None
@@ -133,5 +142,3 @@ def resource(*args, **kwargs):
     See :py:meth:`boto3.session.Session.resource`.
     """
     return _get_default_session().resource(*args, **kwargs)
-
-
